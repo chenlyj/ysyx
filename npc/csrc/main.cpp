85aa8bea42efae8f4758091478ba20a308d4d87c
tracer-ysyx <tracer@ysyx.org>
2023-07-18 00:00:11 +0800

    > sim RTL ysyx_22051761 陈骏昊 Linux cjh-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:00:11 up  2:15,  1 user,  load average: 0.29, 0.18, 0.06

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
-
-int main() {
-  printf("Hello, ysyx!\n");
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated.h"
+#include "Vtop.h"
+#include "verilated_vcd_c.h"
+#define sim_time 200
+int main(int argc,char** argv){
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
+  Vtop* top = new Vtop{contextp};
+  top->trace(m_trace,5);
+  m_trace->open("waveform.vcd");
+  while (contextp->time() < sim_time && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    m_trace->dump(contextp->time());
+    assert(top->f == (a ^ b));
+  }
+  m_trace->close();
+  delete top;
   return 0;
 }
