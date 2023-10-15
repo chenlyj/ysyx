7f3b6f11109698a1a965815f41a75ab741f0cb9c
ysyx_22051761-Chen Junhao <xiaodaome@126.com>
2023-10-15 16:06:30 +0800

    finish building verilator simulation environment

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,39 +7,28 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
-#define sim_time 200
-
-static Vtop dut;
+static Vtop top;
 void nvboard_bind_all_pins(Vtop* top);
 
+void single_cycle() {
+  top.clk = 0;
+  top.eval();
+  top.clk = 1;
+  top.eval();
+}
+void reset(int n) {
+  top.rst = 1;
+  while (n-- > 0) single_cycle();
+  top.rst = 0;  
+}
 int main(int argc, char** argv) {
-  nvboard_bind_all_pins(&dut);
+  nvboard_bind_all_pins(&top);
   nvboard_init();
-  // VerilatedContext* contextp = new VerilatedContext;
-  // contextp->commandArgs(argc, argv);
-  // Verilated::traceEverOn(true);
-  // VerilatedVcdC *m_trace = new VerilatedVcdC;
-  // Vtop* top = new Vtop{contextp};
-  // top->trace(m_trace,5);
-  // m_trace->open("waveform.vcd");
-  // while (contextp->time() < sim_time && !contextp->gotFinish()) {
-  dut.a = 1;
-  dut.b = 0;
-  dut.eval();
+  reset(10);
   while (1) {
     nvboard_update();
-    
-    // contextp->timeInc(1);
-    //  int a = rand() & 1;
-    //  int b = rand() & 1;
-    //  top->a = a;
-    //  top->b = b;
-    //  top->eval();
-    //  printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    //  m_trace->dump(contextp->time());
-    //  assert(top->f == (a ^ b));
+    single_cycle();
   }
-  // m_trace->close();
-  // delete top;
+  nvboard_quit();
   return 0;
 }
